c0e6d25c27e91bd1221e3487911445e14c64780a
mgbaozi <wuhao930301@gmail.com>
2014-03-18 03:17:18 +0800

    index router

--- handlers/index.py
+++ handlers/index.py
@@ -1,7 +1,16 @@
 import tornado.web
+from base.session_tools import SessionTools
 
 class IndexHandler(tornado.web.RequestHandler):
-	@tornado.web.asynchronous
+
+	def __init__(self, application, request, **kwargs):
+		self.session = SessionTools()
+		super(IndexHandler, self).__init__(application, request, **kwargs)
 	def get(self):
-		self.write("It works")
-		self.finish()
+		user_id = self.session.logged_user(self.get_cookie)
+		if not user_id:
+			return self.render('login.html')
+		add_type = self.get_argument("add", None)
+		if not add_type:
+			return self.render('index.html')
+		return self.render("add{0}.html".format(add_type))
