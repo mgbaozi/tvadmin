0b07db7e5d1b8dc3360d7e96c04a6f53d6ba0dc8
mgbaozi <wuhao930301@gmail.com>
2014-03-18 04:26:50 +0800

    debug setting

--- handlers/index.py
+++ handlers/index.py
@@ -1,6 +1,9 @@
 import tornado.web
 from base.session_tools import SessionTools
 
+import logging
+log = logging.getLogger("index")
+log.setLevel(logging.DEBUG)
 class IndexHandler(tornado.web.RequestHandler):
 
 	def __init__(self, application, request, **kwargs):
@@ -8,9 +11,11 @@ class IndexHandler(tornado.web.RequestHandler):
 		super(IndexHandler, self).__init__(application, request, **kwargs)
 	def get(self):
 		user_id = self.session.logged_user(self.get_cookie)
+		log.debug("user_id is {0}".format(user_id))
 		if not user_id:
 			return self.render('login.html')
 		add_type = self.get_argument("add", None)
+		log.debug("get add{0}".format(add_type))
 		if not add_type:
 			return self.render('index.html')
 		return self.render("add{0}.html".format(add_type))
