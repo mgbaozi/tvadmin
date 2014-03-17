c78471e236d6e79d53660bf33a8f322d677d41c9
mgbaozi <wuhao930301@gmail.com>
2014-03-18 07:01:31 +0800

    kinds of bugs

--- models/notices.py
+++ models/notices.py
@@ -1,9 +1,11 @@
 from modelbase import ModelBase
 from datetime import datetime, timedelta
+from users import UserModel
 
 class NoticeModel(ModelBase):
 	def __init__(self):
 		self.name = "notices"
+		self.users = UserModel()
 		super(NoticeModel, self).__init__()
 
 	def add_notice(self, data):
@@ -15,14 +17,25 @@ class NoticeModel(ModelBase):
 				}	
 		return str(self.collection.insert(notice))
 	
+	def to_user(self, notice):
+		user_id = str(notice["publisher"])
+		user = self.users.get_one(user_id)
+		notice["publisher"] = user["name"]
+		notice["create_date"] = notice["create_date"].strftime('%Y-%m-%d')
+		notice["invalid_date"] = notice["invalid_date"].strftime('%Y-%m-%d')
+		return notice
+
 	def get_one(self, notice_id):
 		notice = self.collection.find_one(ModelBase.get_oid(notice_id))
+		notice = self.to_user(notice)
 		return ModelBase.transform_id(notice)
 
 	def get_all(self):
 		cursor = self.collection.find()
-		return ModelBase.cursor2list(cursor)
+		notice_list = ModelBase.cursor2list(cursor)
+		return [self.to_user(notice) for notice in notice_list]
 
 	def get_effective(self):
 		cursor = self.collection.find({'$gte': datetime.utcnow()})
-		return ModelBase.cursor2list(cursor)
+		notice_list = ModelBase.cursor2list(cursor)
+		return [self.to_user(notice) for notice in notice_list]
